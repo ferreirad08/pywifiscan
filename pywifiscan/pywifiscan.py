de16bab8a24b0b5d5ed695ab1e04c377702217fb
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2023-01-20 22:08:43 -0400

    Update pywifiscan.py

--- pywifiscan/pywifiscan.py
+++ pywifiscan/pywifiscan.py
@@ -1,18 +1,24 @@
 from subprocess import check_output
 
 
-def get_interface():
-    return check_output("route | grep default | awk 'NR==1 {print $NF}'", shell=True)\
-        .decode("utf-8").replace("\n", "")
+def get_interface() -> str:
+    try:
+        return check_output("route | grep default | awk 'NR==1 {print $NF}'", shell=True)\
+            .decode("utf-8").replace("\n", "")
+    except:
+        return ""
 
 
-def scan_networks(iface):
-    lines = check_output(f"sudo iwlist {iface} scan | grep -Po '(?<=(ESSID:\")).*(?=\")|(?<=(level=)).*(?= dBm)'", shell=True)\
-        .decode("utf-8").splitlines()
+def scan_networks(iface) -> dict:
+    try:
+        lines = check_output(f"sudo iwlist {iface} scan | grep -Po '(?<=(ESSID:\")).*(?=\")|(?<=(level=)).*(?= dBm)'", shell=True)\
+            .decode("utf-8").splitlines()
 
-    networks = {}
+        networks = {}
 
-    for i in range(1, len(lines), 2):
-        networks[lines[i]] = int(lines[i - 1])
+        for i in range(1, len(lines), 2):
+            networks[lines[i]] = int(lines[i - 1])
 
-    return networks
+        return networks
+    except:
+        return {}
